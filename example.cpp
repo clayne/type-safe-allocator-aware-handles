b8ee9c6046d938984c8cd2f6b70243ddd812b18d
Tednesday <tyler@tymax.com>
2021-12-06 21:15:35 +0000

    new commit

---
+++ example.cpp
@@ -0,0 +1,53 @@
+#include "type_handle.h"
+#include <new>
+
+struct MyData {
+    int a;
+    double b;
+    const char* c = "hello";
+};
+
+int main() {
+
+    typedef simple_allocator nstd::simple_allocator<MyData, 1000>;
+
+    // Create our allocator for this type handle type
+    nstd::handle<MyData, simple_allocator>::allocator = new simple_allocator();
+
+    // Allocate a handle to some data. The handle specifies the type it points to and the allocator responsible 
+    // for the data.
+    typedef HandleMyData nstd::handle<MyData, simple_allocator>;
+
+    // typedefs are useful to get away from angle bracket madness
+    HandleMyData handle = HandleMyData::allocator->allocate();
+
+    // get can get data with ptr() 
+    MyData *ptr_my_data = handle.ptr();
+    // check if its null 
+    if (ptr_my_data == nullptr) {
+        printf("this shouldn't be null since we just allocated it!");
+        delete HandleMyData::allocator;
+        return;
+    }
+
+    // deallocate the handle 
+    HandleMyData::allocator->deallocate(handle);
+    ptr_my_data = handle.ptr();
+    if (ptr_my_data == nullptr) {
+        printf("this should be null since we deallocated it!")
+    }
+
+    // use ref() to get a reference. This will call abort() if its an invalid handle 
+    handle = HandleMyData::allocator->allocate();
+    HandleMyData& my_data = handle.ref();
+
+    // or we can access it via checked execution.
+    // this will check if valid and then execute the functor
+    // if its not valid nothing will happen
+    handle.checked_execution([](MyData *ptr){ *ptr.a = 10; });
+    handle.checked_execution([](MyData *ptr){
+        // pointer is guaranteed to be valid (by your allocator)
+        // at this point
+        *ptr.c = "Hello there"; 
+    });
+}
--- type_handle.h
+++ type_handle.h
@@ -1,6 +1,10 @@
-namespace nstd { // non-standard c++
+#include <type_traits>
+#include <cstdlib>
+#include <cstdint>
 
-    template<typename T, typename Allocator, typename HandleType = int>
+namespace nstd { // non-standard library
+
+    template<typename T, typename Allocator, typename HandleType = uint64_t>
     struct handle {
 
         static_assert(std::is_fundamental<HandleType>());
@@ -36,4 +40,64 @@ namespace nstd { // non-standard c++
     };
     template<typename T, typename Allocator, typename HandleType>
     Allocator* handle<T, Allocator, HandleType>::allocator = nullptr;
+
+    // eventually the counter in a simple allocator has to wrap 
+    // back around. This means it will allocate you handles 
+    // that you may have already stored (zombie references). So bear that in mind
+    // Because the counter is a uint32_t it will take 4294967294 allocations 
+    // before this starts to happen 
+    template<typename T, uint32_t capacity>
+    struct simple_allocator {
+
+        T data[capacity];
+        uint32_t slot[capacity];
+        uint32_t counter = 1;
+
+        simple_allocator() {
+            for (uint64_t i = 0; i < capacity; i++) {
+                slot[i] = 0;
+            }
+        }
+
+        nstd::handle<T, simple_allocator> allocate() {
+            
+            for (uint32_t i = 0; i < capacity; i++) {
+                if (slot[i] == 0) {
+
+                    uint64_t handle = (uint64_t)i << 32;
+                    handle |= (uint64_t)counter;
+                    slot[i] = 1;
+
+                    counter++;      
+                    if (counter == 0) counter++; // it wrapped around, uh oh
+                    
+                    return handle;
+                }
+            }
+
+            return 0; // 0 is an invalid handle
+        }
+
+        void deallocate(nstd::handle<T, simple_allocator> handle) {
+            uint32_t index = handle.id >> 32;
+            if (index >= capacity) return;
+            slot[index] = 0;
+        }
+
+        T* handle_deref(nstd::handle<T, simple_allocator> &handle) {
+
+            if (handle.id == 0) return nullptr; 
+            uint32_t index = handle.id >> 32;
+
+            if (index >= capacity) {
+                handle.id = 0;
+                return nullptr;
+            }
+            if (slot[index] == 0) {
+                handle.id = 0;
+                return nullptr;
+            }
+            return &data[index];
+        }
+    };
 }
