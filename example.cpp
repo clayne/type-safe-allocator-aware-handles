b24399fa6ab07d7062b64daeb955158d8fb2fcf5
Tednesday <tyler@tymax.com>
2021-12-07 07:23:21 +0000

    fixed example

--- example.cpp
+++ example.cpp
@@ -16,7 +16,7 @@ int main() {
 
     // Allocate a handle to some data. The handle specifies the type it points to and the allocator responsible 
     // for the data.
-    typedef HandleMyData nstd::handle<MyData, simple_allocator>;
+    typedef nstd::handle<MyData, simple_allocator> HandleMyData;
 
     // typedefs are useful to get away from angle bracket madness
     HandleMyData handle = HandleMyData::allocator->allocate();
@@ -27,28 +27,28 @@ int main() {
     if (ptr_my_data == nullptr) {
         printf("this shouldn't be null since we just allocated it!");
         delete HandleMyData::allocator;
-        return;
+        return 1;
     }
 
     // deallocate the handle 
     HandleMyData::allocator->deallocate(handle);
     ptr_my_data = handle.ptr();
     if (ptr_my_data == nullptr) {
-        printf("this should be null since we deallocated it!")
+        printf("this should be null since we deallocated it!");
     }
 
     // use ref() to get a reference. This will call abort() if its an invalid handle 
     handle = HandleMyData::allocator->allocate();
-    HandleMyData& my_data = handle.ref();
+    MyData& my_data = handle.ref();
 
     // or we can access it via checked execution.
     // this will check if valid and then execute the functor
     // if its not valid nothing will happen
-    handle.checked_execution([](MyData *ptr){ *ptr.a = 10; });
+    handle.checked_execution([](MyData *ptr){ ptr->a = 10; });
     handle.checked_execution([](MyData *ptr){
         // pointer is guaranteed to be valid (by your allocator)
         // at this point
-        *ptr.c = "Hello there"; 
+        ptr->a = 10;
     });
 
     delete HandleMyData::allocator;
